78e7b2ec550e3699c2fbd67e04c3513d88f969b0
Kofler Phillip <kofelrp@edu.htl-villach.at>
2026-05-16 00:54:25 +0200

    added initial express config, health checks and logging

---
+++ app/server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const __dirname = import.meta.dirname;
+
+/* ***************** IMPORT packages *********************** */
+import express from 'express';
+import { createServer } from 'http';
+import path from 'path';
+
+/* ***************** IMPORT LIBS *************************** */
+import { logger } from './utils/logger.js';
+import { setupHealthChecks } from './utils/health-checks.js';
+
+/* ***************** IMPORT REQUEST-HANDLER **************** */
+
+/* ***************** CONFIG and CONSTS ********************* */
+/* Take configuration from environment variables or use hardcoded default value */
+const HOSTNAME = process.env.BINDADDRESS || '0.0.0.0';
+const PORT = process.env.PORT || 8080;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || 'mongodb://127.0.0.1/team_a';
+const MONGODB_RECREATE = process.env.MONGODB_RECREATE === 'true';
+
+/* ***************** START UP ******************************* */
+logger.info('Backend - Starting configuration...');
+
+const app = express();
+app.use(
+    express.json({
+        type: ['application/json', 'application/merge-patch+json'],
+        limit: '50mb',
+    }),
+);
+
+// use build folder of vite as static directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// create HTTP server
+logger.info('Backend - Starting up ...');
+const httpServer = createServer(app);
+
+// setup health check endpoints on server
+setupHealthChecks(httpServer);
+
+// start listening to HTTP requests
+httpServer.listen(PORT, HOSTNAME, () => {
+    logger.info(`Backend - Running on port ${PORT}...`);
+});
+
+export default httpServer;
