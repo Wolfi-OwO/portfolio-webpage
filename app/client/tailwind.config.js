44a9a7ec965b69b92b2904d13b4a329819481940
Kofler Phillip <kofelrp@edu.htl-villach.at>
2026-05-20 19:48:25 +0200

    added safelist to tailwind config to prevent purging of dynamic classes

--- app/client/tailwind.config.js
+++ app/client/tailwind.config.js
@@ -5,5 +5,12 @@ export default {
     theme: {
         extend: {},
     },
+    safelist: [
+        {
+            pattern:
+                /^(bg|text|border|ring)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)(\/\d+)?$/,
+            variants: ['dark', 'hover', 'focus'],
+        },
+    ],
     plugins: [],
 };
