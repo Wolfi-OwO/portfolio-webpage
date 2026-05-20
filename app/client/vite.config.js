a1e3b2817cdc36f24c1923bdb2b1ac0be4a33510
Kofler Phillip <kofelrp@edu.htl-villach.at>
2026-05-20 23:08:34 +0200

    added vite build options to disable source maps and set target to es2020

--- app/client/vite.config.js
+++ app/client/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    css: {
+        devSourcemap: false,
+    },
+    build: {
+        target: 'es2020',
+        sourcemap: false,
+    },
     server: {
         proxy: {
             '/api': {
