d1fc3f9fcb41f1211b999e128681cfbcb0420aaf
Kofler Phillip <kofelrp@edu.htl-villach.at>
2026-05-20 23:08:23 +0200

    Define color palette for technologies and programming languages in Tailwind config

--- app/client/tailwind.config.js
+++ app/client/tailwind.config.js
@@ -2,15 +2,35 @@
 export default {
     darkMode: 'class',
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    experimental: {},
     theme: {
         extend: {},
     },
     safelist: [
         {
             pattern:
-                /^(bg|text|border|ring)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)(\/\d+)?$/,
-            variants: ['dark', 'hover', 'focus'],
+                /^(bg|text|border|ring)-(emerald|indigo|sky|green|neutral|cyan|yellow|blue|red|orange|purple|pink|slate)-(50|100|200|300|400|500|600|700|800|900|950)(\/\d+)?$/,
+            variants: ['dark'],
         },
     ],
     plugins: [],
 };
+
+/*
+Farbideen für Technologien / Programmiersprachen:
+
+emerald -> Python
+indigo  -> Streamlit / Next.js
+sky     -> React
+green   -> Node.js
+neutral -> Express
+cyan    -> TailwindCSS
+
+yellow  -> JavaScript
+blue    -> TypeScript / Docker
+red     -> Java / Angular
+orange  -> HTML / Git
+purple  -> C# / GraphQL
+pink    -> SASS / Prisma
+slate   -> SQL / Backend Tools
+*/
