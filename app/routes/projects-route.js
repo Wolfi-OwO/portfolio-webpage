6c7e61c82b369eb83b8292bee1464803b3fdfe65
Kofler Phillip <kofelrp@edu.htl-villach.at>
2026-05-28 12:20:19 +0200

    added better comments

--- app/routes/projects-route.js
+++ app/routes/projects-route.js
@@ -9,15 +9,19 @@ import {
     updateProjectById,
     deleteProjectById,
 } from '../handlers/projects-handlers.js';
+
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 /* ***************** CONFIG and CONSTS ********************* */
 const projectsRouter = express.Router();
 
-projectsRouter.get('', getAllProjects);
-projectsRouter.get('{id}', getProjectById);
-projectsRouter.post('', authMiddleware, createNewProject);
-projectsRouter.put('{id}', authMiddleware, updateProjectById);
-projectsRouter.delete('{id}', authMiddleware, deleteProjectById);
+/* ***************** PUBLIC ROUTES ************************* */
+projectsRouter.get('/', getAllProjects);
+projectsRouter.get('/:id', getProjectById);
+
+/* ***************** PROTECTED ROUTES ********************** */
+projectsRouter.post('/', authMiddleware, createNewProject);
+projectsRouter.put('/:id', authMiddleware, updateProjectById);
+projectsRouter.delete('/:id', authMiddleware, deleteProjectById);
 
 export { projectsRouter };
--- app/routes/technologies-route.js
+++ app/routes/technologies-route.js
@@ -1,5 +1,7 @@
+/* ***************** IMPORT packages *********************** */
 import express from 'express';
 
+/* ***************** IMPORT REQUEST-HANDLER **************** */
 import {
     getAllTechnologies,
     getTechnologyById,
@@ -9,12 +11,16 @@ import {
 } from '../handlers/technologies-handlers.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 
+/* ***************** CONFIG and CONSTS ********************* */
 const technologiesRouter = express.Router();
 
-technologiesRouter.get('', getAllTechnologies);
-technologiesRouter.get('{id}', getTechnologyById);
-technologiesRouter.post('', authMiddleware, createTechnology);
-technologiesRouter.put('{id}', authMiddleware, updateTechnologyById);
-technologiesRouter.delete('{id}', authMiddleware, deleteTechnologyById);
+/* ***************** PUBLIC ROUTES ************************* */
+technologiesRouter.get('/', getAllTechnologies);
+technologiesRouter.get('/:id', getTechnologyById);
+
+/* ***************** PROTECTED ROUTES ********************** */
+technologiesRouter.post('/', authMiddleware, createTechnology);
+technologiesRouter.put('/:id', authMiddleware, updateTechnologyById);
+technologiesRouter.delete('/:id', authMiddleware, deleteTechnologyById);
 
 export { technologiesRouter };
