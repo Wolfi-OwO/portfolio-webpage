4a7d88b1a89b183a1e41775a0d75985d37a14558
Wolfi-OwO <koflerphillip@outlook.com>
2026-07-11 22:37:01 +0200

    feat: add services page route to the application

--- application/client/src/routes.jsx
+++ application/client/src/routes.jsx
@@ -3,6 +3,7 @@ import DefaultLayout from './layouts/default-layout/default-layout.jsx';
 import Homepage from './pages/homepage/homepage.jsx';
 import ProjectsPage from './pages/projects-page/projects-page.jsx';
 import ContactPage from './pages/contact-page/contact-page.jsx';
+import ServicesPage from './pages/services-page/services-page.jsx';
 import PrivacyPolicyPage from './pages/privacy-policy-page/privacy-policy-page.jsx';
 import AdminLoginPage from './pages/admin-login-page/admin-login-page.jsx';
 import ErrorPage from './pages/error-page/error-page.jsx';
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: '/projects',
                 element: <ProjectsPage />,
+            },
+            {
+                path: '/services',
+                element: <ServicesPage />,
             },
             {
                 path: '/contact',
